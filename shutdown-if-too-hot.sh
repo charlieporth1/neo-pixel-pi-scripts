779916a3af11bc4ebdb47d914e201da0e4257594
Charlie Porth <charlieporth1@gmail.com>
2023-10-21 10:51:08 -0500

    Fixed systemd to fork and script

--- shutdown-if-too-hot.sh
+++ shutdown-if-too-hot.sh
@@ -4,6 +4,16 @@ temp=`cpu-temp.sh | tr -dc '0-9'`
 other_processes=`ps -aux | grep '.py' | grep -v "grep" | grep -o '.py'`
 max_temp=550
 arg=$1
+function run() {
+	array=($@)
+	args=${array[@]:2:99}
+	bin=$1
+	sudo killall -9 python3
+	pgrep -f $bin | xargs sudo kill -9
+	$bin $args &
+	pgrep -f $bin > /run/led-ctp.pid
+	return 0
+}
 if [[ $arg = --off ]]; then
 	sudo killall -9 python3
 	pgrep -f led-off.py | xargs sudo kill -9
@@ -12,31 +22,24 @@ if [[ $arg = --off ]]; then
 fi
 if [[ $temp -ge $max_temp ]]; then
 	echo "Shutting down computer too hot"
-	sudo killall -9 python3
-	pgrep -f led-off.py | xargs sudo kill -9
-	sudo led-off.py --hot
+	run led-off.py --hot
 #	timeout 15 sudo poweroff
 #	timeout 20 sudo poweroff -f
 elif [[ $(( $max_temp - 200 )) -le $temp  ]] && ! pgrep -f color-cycle.py; then
 	echo "No other process; starting new"
-	(
 #		sudo color-fade.py 
 #		 sudo color-cycle.py --win95 --white --more -c --time 50
 		 #sudo color-cycle.py --steps=150 --more --slow --time=15 -c
 #o		sudo color-cycle.py --win95 --white --more -c --time 5 --steps 145 
-	      	nowhour=$(date +%H | bc -l)
-        	if [[ $nowhour -ge 6 ]]; then
-			echo "No other process; starting new"
-        	         sudo color-cycle.py --steps=150 --more --slow --time=15 -c &
-		else
-			sudo killall -9 python3
-			pgrep -f led-off.py | xargs sudo kill -9
-			sudo led-off.py --off
-        	fi
-	)&
+      	nowhour=$(date +%H | bc -l)
+       	if [[ $nowhour -ge 6 ]]; then
+		echo "No other process; starting new"
+       	        run color-cycle.py --steps=150 --more --slow --time=15 -c
+	else
+		run led-off.py --off
+       	fi
+
 else
-	sudo killall -9 python3
-	pgrep -f led-off.py | xargs sudo kill -9
-	sudo led-off.py --off
+	run led-off.py --off
 	echo "Not enought"
 fi
