768d51886218602fd8d4a63f681174cbdb4fa818
Charlie Porth <charlieporth1@gmail.com>
2025-09-20 15:07:04 -0500

    added timers to ctp-led.sh

--- ctp-led.sh
+++ ctp-led.sh
@@ -5,6 +5,8 @@ case $arg
 in
 	--start )
 		systemctl start ctp-led-run.service
+		systemctl enable ctp-led-run.timer ctp-led-stop.timer ctp-led-holiday.timer
+		systemctl start ctp-led-run.timer ctp-led-stop.timer ctp-led-holiday.timer
 	;;
 	--stop )
 		systemctl stop ctp-led-run.service
